2c2a61751af9f003749228ff08e14e0f196a29b4
Ciamuthama <muthamapeter79@gmail.com>
2023-08-15 17:53:40 +0300

    Adding a Modal

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Text, View,FlatList, TouchableOpacity,ImageBackground } from 'react-native';
+import {StyleSheet, Text, View,FlatList, TouchableOpacity,ImageBackground, Modal } from 'react-native';
 import { globalStyles } from '../styles/globalstyles';
+import {MaterialIcons} from '@expo/vector-icons'
 import Card from '../shared/cards';
 
 
@@ -13,8 +14,31 @@ export default function Home({ navigation }) {
         {title: 'Fast and Furious 7', rating: 1, body:'Come out in 2020', key: '4'},
     ])
 
+    const [open, setOpen]= useState(false)
+
     return (
         <ImageBackground source={require('../assets/game_bg.png')} styles={globalStyles.container}>
+
+            <Modal visible={open} animationType='fade'>
+                <View style={styles.modal}>
+                    <Text>Hello</Text>
+                </View>
+                <MaterialIcons
+                name='close'
+                size={24}
+                color='#000'
+                style={styles.ModalToggle}
+                onPress={()=> setOpen(false)}
+            />
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                color='#000'
+                style={styles.ModalToggle}
+                onPress={()=> setOpen(true)}
+            />
             <FlatList data={review} renderItem={({ item }) => (
                 <TouchableOpacity onPress={() => navigation.navigate("Review", item)}>
                     <View style={globalStyles.wrapper}>
@@ -28,3 +52,20 @@ export default function Home({ navigation }) {
         </ImageBackground>
     )
 }
+
+const styles = StyleSheet.create({
+    modal: {
+        alignItems:'center',
+        flex: 1,
+         
+    },
+    ModalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        borderRadius: 5,
+        alignSelf: 'center',
+        marginTop: 10,
+        padding:5,
+    }
+})
